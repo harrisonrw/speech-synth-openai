705b24062efaa16d0cdd271164e649cdfa777aaa
Robert Harrison <harrisonrw@gmail.com>
2024-09-09 09:04:03 -0700

    Initial Xcode Project

---
+++ SpeechSynthOpenAI/SpeechSynthOpenAI/SpeechSynthOpenAIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpeechSynthOpenAIApp.swift
+//  SpeechSynthOpenAI
+//
+//  Created by Robert Harrison on 9/9/24.
+//
+
+import SwiftUI
+
+@main
+struct SpeechSynthOpenAIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
